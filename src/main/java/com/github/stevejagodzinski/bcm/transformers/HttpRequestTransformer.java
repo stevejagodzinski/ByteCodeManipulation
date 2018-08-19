e5530e3c0686463545f64a8a6d8946f08f0eed45
Steve Jagodzinski <steve.jagodzinski>
2018-08-19 01:39:24 -0400

    Instrument the HTTP response to include a unique ID

--- src/main/java/com/github/stevejagodzinski/bcm/transformers/HttpRequestTransformer.java
+++ src/main/java/com/github/stevejagodzinski/bcm/transformers/HttpRequestTransformer.java
@@ -1,8 +1,11 @@
 package com.github.stevejagodzinski.bcm.transformers;
 
 import javassist.ByteArrayClassPath;
+import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.bytecode.AccessFlag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +31,8 @@ public class HttpRequestTransformer implements ClassFileTransformer {
                 CtClass ctClass = pool.get(TARGET_CLASS_DOT);
                 if (!ctClass.isFrozen()) {
                     // TODO: Add specific transformations to the ctClass
+                    CtMethod serviceMethod = findServiceMethod(ctClass);
+                    addUniqueIdToResponse(serviceMethod);
                     transformed = ctClass.toBytecode();
                 } else {
                     LOG.error("Class has already been loaded or written out and thus it cannot be modified any more. className={}", className);
@@ -37,5 +42,24 @@ public class HttpRequestTransformer implements ClassFileTransformer {
             LOG.error("Error transforming class", e);
         }
         return transformed;
+    }
+
+    private CtMethod findServiceMethod(CtClass httpServletClass) throws NoSuchMethodException {
+        for(CtMethod method : httpServletClass.getDeclaredMethods()) {
+            if (AccessFlag.isProtected(method.getMethodInfo().getAccessFlags()) && method.getName().equals("service")) {
+                return method;
+            }
+        }
+
+        throw new NoSuchMethodException("Can not find a method named 'service' with 'protected' access in class HttpServlet");
+    }
+
+    private void addUniqueIdToResponse(CtMethod serviceMethod) throws CannotCompileException {
+        serviceMethod.insertAfter(generateAddUniqueIdToResponseJavaCode());
+    }
+
+    private String generateAddUniqueIdToResponseJavaCode() {
+        // TODO: Make key configurable
+        return "$2.addHeader(\"X-SJ-UUID\", UUID.randomUUID().toString());";
     }
 }
